aefe22770413d499f8650895d02388d637ea392f
psv <skliarpawlo@rambler.ru>
2012-11-06 17:29:42 +0200

    templates config

--- ganymede/settings.py
+++ ganymede/settings.py
@@ -1,14 +1,15 @@
 # Django settings for ganymede project.
 from core import db
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+BASE_PATH = os.path.join(os.path.dirname(__file__), "../" )
+
 ADMINS = (
     ('Pavlo Skliar', 'skliarpawlo@rambler.ru'),
 )
-
-MANAGERS = ADMINS
 
 db.init()
 
@@ -24,30 +25,11 @@ USE_L10N = False
 
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_PATH, "assets")
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_PATH, "ganymede", "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -84,9 +66,7 @@ ROOT_URLCONF = 'ganymede.urls'
 WSGI_APPLICATION = 'ganymede.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join( BASE_PATH, 'templates' ),
 )
 
 INSTALLED_APPS = (
@@ -95,12 +75,8 @@ INSTALLED_APPS = (
     #'django.contrib.sessions',
     #'django.contrib.sites',
     #'django.contrib.messages',
-    #'django.contrib.staticfiles',
 
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.staticfiles',
     'tests.seo_texts',
 )
 
--- ganymede/urls.py
+++ ganymede/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.static import *
+from django.conf import settings
 
 urlpatterns = patterns('',
-    #url(r'^seo_texts/', include('seo_texts.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^$', 'ganymede.views.home')
 )
