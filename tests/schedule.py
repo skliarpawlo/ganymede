15468977f01a3dd7824af546e8dbc80bdf84deca
psv <skliarpawlo@rambler.ru>
2012-11-22 17:25:19 +0200

    schedule with capturing pid

--- tests/schedule.py
+++ tests/schedule.py
@@ -1,11 +1,22 @@
 import sched
 import time
-from tests import tests_config
-from tests import tasks
+import tests_config
+import utils
+import tasks
+import core.lock
+
+def run(test_id) :
+    pid_file = utils.pid_file(test_id)
+    test_pid = core.lock.is_free(pid_file)
+    if (test_pid==0):
+        core.lock.capture(pid_file)
+        tasks.run_test(test_id)
+        core.lock.uncapture(pid_file)
+
 
 scheduler = sched.scheduler(time.time, time.sleep)
 
 for x in tests_config.all_tests:
-    scheduler.enter(1, 1, tasks.run_test, (x,))
+    scheduler.enter(1, 1, run, (x,))
 
 scheduler.run()
