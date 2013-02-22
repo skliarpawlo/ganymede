22af558b8d59987bd49e04e0243d77476ec9dd6c
pavlo <skliarpawlo@rambler.ru>
2013-02-22 15:50:11 +0200

    USR1 signal handling

--- testing_runtime/tasks.py
+++ testing_runtime/tasks.py
@@ -7,12 +7,16 @@ import core.path
 import core.db
 import core.git
 import core.logger
+import core.browser
+import core.vscreen
 import ganymede.settings
 import os
 from testing_runtime import models
 from sqlalchemy.orm.exc import NoResultFound
 import json
 from testlib import utils
+import sys
+import signal
 
 def run_test(test):
     "запускает PageTest"
@@ -63,21 +67,44 @@ def run_task( task ) :
 
     return result
 
+def signal_handler( task ) :
+    def stop_me( signum, frame ) :
+        core.logger.write("task interrupted!", task_id=task.task_id)
+        task.artifacts = json.dumps( core.logger.get_artifacts(task.task_id) )
+        task.log = core.logger.read(task.task_id)
+        task.status = u"fail"
+        core.db.session.query(models.Task)\
+        .filter(models.Task.task_id == task.task_id)\
+        .update( {"status" : task.status, "log" : task.log, "artifacts" : task.artifacts } )
+
+        pid_dir = os.path.join( ganymede.settings.HEAP_PATH, "cron")
+        pid_file = os.path.join( pid_dir, "cron.pid" )
+        core.lock.uncapture( pid_file )
+
+        core.browser.stop()
+        core.vscreen.stop()
+        core.db.close()
+
+        sys.exit()
+
+    signal.signal( signal.SIGUSR1, stop_me )
+
 def run_any() :
     pid_dir = os.path.join( ganymede.settings.HEAP_PATH, "cron")
     core.path.ensure( pid_dir )
     pid_file = os.path.join( pid_dir, "cron.pid" )
     pid = core.lock.is_free( pid_file )
-    if (pid == 0) :
+    if pid == 0 :
         core.lock.capture(pid_file)
         try :
             task = core.db.session.query(models.Task).filter(models.Task.status=='waiting').order_by(models.Task.add_time).limit(1).one()
             core.db.execute( "UPDATE gany_tasks SET status='running' WHERE task_id={0}".format( str(task.task_id) ) )
             try :
+                signal_handler( task )
                 result = run_task( task )
             except :
                 result = False
-                core.logger.write(u"Exception raised: " + traceback.format_exc(), task_id=task.task_id)
+                core.logger.write(u"Exception raised: {0}".format(traceback.format_exc()), task_id=task.task_id)
             finally :
                 task.artifacts = json.dumps( core.logger.get_artifacts(task.task_id) )
                 task.log = core.logger.read(task.task_id)
